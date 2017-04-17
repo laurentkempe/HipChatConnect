fe4fbfadcbc2b7c434f863948e0d62304538699b
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2017-04-17 15:48:15 +0200

    Add configuration to IOC

--- src/HipChatConnect/Startup.cs
+++ src/HipChatConnect/Startup.cs
@@ -36,6 +36,8 @@ namespace HipChatConnect
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_ => Configuration);
+
             services.Configure<AppSettings>(settings =>
             {
                 settings.BaseUrl = Configuration["BASE_URL"];
